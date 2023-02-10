59f705d0803b71e1f74579df5c87599b006b05b9
MizanSafin <drmizanurrahman452@gmail.com>
2023-02-10 11:06:48 +0000

    3rd commit

--- main.js
+++ main.js
@@ -11,11 +11,16 @@ if (lsDataArr) {
 } else {
   dataArr = [];
 }
-
+let splicedItem;
+let indexV;
 function handleFormSubmit() {
-  const formData = fetchDataFromForm();
-  sendDataToLS(formData);
-  showDataToTable();
+  if (splicedItem == undefined) {
+    const formData = fetchDataFromForm();
+    sendDataToLS(formData);
+    showDataToTable();
+  } else {
+    updateData();
+  }
   resetForm();
 }
 
@@ -43,6 +48,7 @@ function showDataToTable() {
                <td>${item.exp}</td>
                <td>
                     <i onclick=deleteItem(${index})  class="fas fa-trash"></i>
+                    <i onclick=editItem(${index})  class="fas fa-edit"></i>
                </td>
             </tr>
         `;
@@ -57,8 +63,61 @@ function deleteItem(index) {
   showDataToTable();
 }
 
+function editItem(index) {
+  indexV = index;
+  splicedItem = dataArr.splice(index, 1);
+  nameEl.value = splicedItem[0].name;
+  jobEl.value = splicedItem[0].job;
+  expEl.value = splicedItem[0].exp;
+  localStorage.setItem("dataArr", JSON.stringify(dataArr));
+  console.log(dataArr);
+}
+
+function updateData() {
+  splicedItem[0].name = nameEl.value;
+  splicedItem[0].job = jobEl.value;
+  splicedItem[0].exp = expEl.value;
+  dataArr.splice(indexV, 0, splicedItem[0]);
+  localStorage.setItem("dataArr", JSON.stringify(dataArr));
+  showDataToTable();
+}
+
 function resetForm() {
   nameEl.value = "";
   jobEl.value = "";
   expEl.value = "";
 }
+
+//Praccticing or problem solving
+// let personsArr = [
+//   {
+//     name: "Mizan",
+//     age: 28,
+//   },
+//   {
+//     name: "Sadman",
+//     age: 1,
+//   },
+//   {
+//     name: "Sahanaj",
+//     age: 2,
+//   },
+// ];
+// let index = 1;
+// const spliceItem = personsArr.splice(1, 1);
+// spliceItem[0].age = 2;
+
+// personsArr.splice(index, 0, spliceItem[0]);
+
+// console.log(personsArr);
+// const newArr = personsArr.map((item,index) => {
+//   if (item.age === 1) {
+//     return { name: "Sadman", age: 2 };
+//   } else {
+//     return item;
+//   }
+// });
+
+// personsArr = newArr;
+// console.log(personsArr);
+// console.log(newArr);
